3134b5127a7607d7f52b3f042eb0cf4914daffcf
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-29 18:43:46 +0300

    refactor: app refactored :package:

--- app/src/main/java/com/example/composealltechniques/composetech/Animation.kt
+++ app/src/main/java/com/example/composealltechniques/composetech/Animation.kt
@@ -1,27 +1,19 @@
 package com.example.composealltechniques.composetech
 
 import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.DurationBasedAnimationSpec
-import androidx.compose.animation.core.Easing
-import androidx.compose.animation.core.FastOutLinearInEasing
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.animateValue
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.slideIn
-import androidx.compose.animation.slideInVertically
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -30,11 +22,21 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.example.composealltechniques.R
 
 @Composable
-fun Animation() {
+fun AnimationSection() {
+    AnimationItem1()
+    AnimationItem2()
+}
+
+@Composable
+fun AnimationItem1() {
 
     var sizeState by remember {
         mutableStateOf(200.dp)
@@ -69,5 +71,79 @@ fun Animation() {
         Button(onClick = { sizeState += 50.dp }) {
             Text("Increase Size")
         }
+    }
+}
+
+@Composable
+fun AnimationItem2() {
+
+    var isHuge by remember { mutableStateOf(false) }
+    var isNeedChangeToColor by remember { mutableStateOf(false) }
+
+    val startColor = Color.Red
+    val endColor = Color.Cyan
+
+    val dp by animateDpAsState(
+        targetValue = if (isHuge) 300.dp else 100.dp,
+        animationSpec = tween(400, 400),
+        label = "Dp huge"
+    )
+    val color by animateColorAsState(
+        targetValue = if (isNeedChangeToColor) endColor else startColor,
+        label = ""
+    )
+
+    var isRotated by remember {
+        mutableStateOf(false)
+    }
+
+    val rotate by animateFloatAsState(
+        targetValue = if (isRotated) 360f else 0f,
+        animationSpec = tween(durationMillis = 2000),
+        label = ""
+    )
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        /*
+        BoxSize(dp = dp, color = color)
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = {
+            isHuge = !isHuge
+        }) {
+            Text(text = "Change to Box Dp")
+        }
+        Button(onClick = {
+            isNeedChangeToColor = !isNeedChangeToColor
+        }) {
+            Text(text = "Rainbow!")
+        } */
+        Image(
+            painter = painterResource(id = R.drawable.fan),
+            contentDescription = "",
+            modifier = Modifier
+                .rotate(rotate)
+                .size(200.dp)
+        )
+        Button(onClick = {
+            isRotated = !isRotated
+        }) {
+            Text(text = "Rotate Fan")
+        }
+    }
+
+    @Composable
+    fun BoxSize(
+        dp: Dp,
+        color: Color
+    ) {
+        Box(
+            modifier = Modifier
+                .size(dp)
+                .background(color)
+        )
     }
 }
---
+++ app/src/main/java/com/example/composealltechniques/meditationUI/MeditationApp.kt
@@ -0,0 +1,60 @@
+package com.example.composealltechniques.meditationUI
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
+import com.example.composealltechniques.ConstraintIds
+
+@Composable
+fun MeditationApp() {
+
+// State Lesson -> StateLessonComponent()
+// Modifier -> ModifierMembers()
+// Image Card -> ImageComponent()
+
+    val constraints = ConstraintSet {
+        val greenBox = createRefFor(ConstraintIds.greenBoxId)
+        val redBox = createRefFor(ConstraintIds.redboxId)
+
+        constrain(greenBox) {
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
+        }
+        constrain(redBox) {
+            top.linkTo(parent.top)
+            start.linkTo(greenBox.end)
+            end.linkTo(parent.end)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
+        }
+        createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.Packed)
+    }
+
+    ConstraintLayout(
+        // Constraint Layout -> Çokça iç içe giren column row kullanmaya bir alternatiftir.
+        constraints,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Color.Green)
+                .layoutId(ConstraintIds.greenBoxId)
+        )
+        Box(
+            modifier = Modifier
+                .background(Color.Red)
+                .layoutId(ConstraintIds.redboxId)
+        )
+    }
+}
